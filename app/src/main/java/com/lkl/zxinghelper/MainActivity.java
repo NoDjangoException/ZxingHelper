f9799775cd67ca0fc963d671b12619ffdc3b0ec8
django <382575810@qq.com>
2017-08-15 16:52:45 +0800

    将调用zxing扫码的demo上传

---
+++ app/src/main/java/com/lkl/zxinghelper/MainActivity.java
@@ -0,0 +1,43 @@
+package com.lkl.zxinghelper;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.landi.zxinglibrary.activity.TestActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+	private TextView mTvResult;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		findViewById(R.id.btn_scan).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				startActivityForResult(new Intent(MainActivity.this,TestActivity.class),TestActivity.REQUEST_CODE);
+			}
+		});
+		mTvResult = (TextView)findViewById(R.id.tv_result);
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if (requestCode == TestActivity.REQUEST_CODE){
+			if (data == null){
+				return;
+			}
+			String result = data.getStringExtra("result");
+			if (result!=null){
+				mTvResult.setText("scan result："+result);
+			}else {
+				mTvResult.setText("can`t receive result");
+			}
+		}
+	}
+}
